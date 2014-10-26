830b3171851851815d67e3ed47371891e0a6439a
zhengfuzhe2013 <yy_you_xiang@163.com>
2014-10-26 18:21:14 +0800

    m

--- Skea/PersonController/PersonViewController.m
+++ Skea/PersonController/PersonViewController.m
@@ -7,13 +7,17 @@
 //
 
 #import "PersonViewController.h"
+#import "TestingViewController.h"
 
-@interface PersonViewController ()
+@interface PersonViewController ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 
 @end
 
 @implementation PersonViewController
-
+{
+    UITextField * _nickNameTextField;
+    UIImageView * photoImageView;
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,7 +31,218 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.navigationItem.titleView = [[Theam currentTheam] navigationTitleViewWithTitle:nil];
+    self.navigationItem.leftBarButtonItem = [[Theam currentTheam] navigationBarLeftButtonItemWithImage:IMG(@"back-cross.png") Title:nil Target:self Selector:@selector(btBack_DisModal:)];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, self.view.frame.size.height) style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+
+    
 }
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row == 0)
+        return 240.f;
+    if(indexPath.row == 1)
+        return 40.f;
+    return 400.f;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString * cellIdentifier = [NSString stringWithFormat:@"cell_%ld",(long)indexPath.row];
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    if(indexPath.row == 0)
+    {
+        UIView * lineView = [[UIView alloc] init];
+        lineView.frame = CGRectMake(0, 20, self.view.frame.size.width, 3);
+        lineView.backgroundColor = [UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1.f];
+        [cell.contentView addSubview:lineView];
+        
+        UILabel * roundLabel = [[UILabel alloc] init];
+        roundLabel.frame = CGRectMake(40, -7.5, 15, 15);
+        roundLabel.backgroundColor = [UIColor blueColor];
+        roundLabel.text = @"1";
+        roundLabel.font = [UIFont systemFontOfSize:13];
+        roundLabel.layer.cornerRadius = 7.5;
+        roundLabel.layer.masksToBounds = YES;
+        roundLabel.textColor = [UIColor whiteColor];
+        roundLabel.textAlignment = NSTextAlignmentCenter;
+        [lineView addSubview:roundLabel];
+        
+        UILabel * roundLabel2 = [[UILabel alloc] init];
+        roundLabel2.frame = CGRectMake(self.view.frame.size.width - 43, -7.5, 15, 15);
+        roundLabel2.backgroundColor = [UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1.f];
+        roundLabel2.text = @"2";
+        roundLabel2.font = [UIFont systemFontOfSize:13];
+        roundLabel2.layer.cornerRadius = 7.5;
+        roundLabel2.layer.masksToBounds = YES;
+        roundLabel2.textColor = [UIColor whiteColor];
+        roundLabel2.textAlignment = NSTextAlignmentCenter;
+        [lineView addSubview:roundLabel2];
+        
+        UILabel * label = [[UILabel alloc] init];
+        label.frame = CGRectMake(30, lineView.frame.origin.y + lineView.frame.size.height + 8, 60, 20);
+        label.backgroundColor = [UIColor clearColor];
+        label.textColor = [UIColor blueColor];
+        label.font = [UIFont systemFontOfSize:13];
+        label.text = NSLocalizedString(@"填写资料", nil);
+        [cell.contentView addSubview:label];
+        
+        UILabel * noticeLabel = [[UILabel alloc] init];
+        noticeLabel.frame = CGRectMake(0, label.frame.origin.y + label.frame.size.height + 10, self.view.frame.size.width, 30);
+        noticeLabel.text = NSLocalizedString(@"恭喜你注册成功!请填写资料", nil);
+        noticeLabel.font = [UIFont systemFontOfSize:13];
+        noticeLabel.backgroundColor = [UIColor clearColor];
+        noticeLabel.textAlignment = NSTextAlignmentCenter;
+        [cell.contentView addSubview:noticeLabel];
+        
+        photoImageView = [[UIImageView alloc] init];
+        photoImageView.frame = CGRectMake(80 + 40, noticeLabel.frame.origin.y + noticeLabel.frame.size.height + 8, 80, 80);
+        photoImageView.layer.cornerRadius = 40.f;
+        photoImageView.layer.masksToBounds = YES;
+        photoImageView.image = [UIImage imageNamed:@"icon-info.png"];
+        photoImageView.userInteractionEnabled = YES;
+        [cell.contentView addSubview:photoImageView];
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(0, 0, 80, 80);
+        btn.layer.cornerRadius = 40.f;
+        [btn addTarget:self action:@selector(photoSelect) forControlEvents:UIControlEventTouchUpInside];
+        [photoImageView addSubview:btn];
+        
+        UILabel * notiLabel = [[UILabel alloc] init];
+        notiLabel.backgroundColor = [UIColor clearColor];
+        notiLabel.frame = CGRectMake(photoImageView.frame.origin.x, photoImageView.frame.origin.y + photoImageView.frame.size.height + 8, photoImageView.frame.size.width - 20, 20);
+        notiLabel.font = [UIFont systemFontOfSize:13.f];
+        notiLabel.text = NSLocalizedString(@"上传头像", nil);
+        [cell.contentView addSubview:notiLabel];
+        
+    }
+    else if (indexPath.row == 1)
+    {
+        UILabel * label = [[UILabel alloc] init];
+        label.frame = CGRectMake(40, 5, 50, 30);
+        label.backgroundColor = [UIColor clearColor];
+        label.text = NSLocalizedString(@"昵称", nil);
+        [cell.contentView addSubview:label];
+        
+        _nickNameTextField = [[UITextField alloc] init];
+        _nickNameTextField.frame = CGRectMake(label.frame.size.width + label.frame.origin.x, 5, self.view.frame.size.width - label.frame.origin.x - label.frame.size.width, 30);
+        _nickNameTextField.delegate = self;
+        _nickNameTextField.placeholder = NSLocalizedString(@"请输入昵称", nil);
+        _nickNameTextField.returnKeyType = UIReturnKeyDone;
+        [cell.contentView addSubview:_nickNameTextField];
+    }
+    else
+    {
+        UIButton * leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        leftButton.frame = CGRectMake(40, 100, 40, 30);
+        [leftButton setTitle:NSLocalizedString(@"退出", nil) forState:UIControlStateNormal];
+        [leftButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [leftButton addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        [cell.contentView addSubview:leftButton];
+        
+        UIButton * rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        rightButton.frame = CGRectMake(self.view.frame.size.width - 40 - 40, 100, 40, 30);
+        [rightButton setTitle:NSLocalizedString(@"提交", nil) forState:UIControlStateNormal];
+        [rightButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [rightButton addTarget:self action:@selector(submit) forControlEvents:UIControlEventTouchUpInside];
+        [cell.contentView addSubview:rightButton];
+        
+        
+    }
+    return cell;
+}
+
+-(void)back
+{
+    [self btBack_DisModal:nil];
+}
+
+-(void)submit
+{
+    TestingViewController * tvc = [[TestingViewController alloc] init];
+    UINavigationController * nvc = [[UINavigationController alloc] initWithRootViewController:tvc];
+    [self presentViewController:nvc animated:YES completion:nil];
+}
+
+-(void)photoSelect
+{
+    NSLog(@"选择投降");
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [imagePicker setAllowsEditing:YES];
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)img editingInfo:(NSDictionary *)editInfo
+{
+    
+    CGSize size = img.size;
+    CGFloat ratio = 1;
+    if (size.width>440) {
+        ratio = 440/size.width;
+    }
+    int ratwid = ratio*size.width;
+    int rathig = ratio*size.height;
+    if (ratwid<1) {
+        ratwid = 1;
+    }
+    if (rathig<1) {
+        rathig = 1;
+    }
+    
+    CGRect rect = CGRectMake(0.0, 0.0, ratwid, rathig);
+    UIGraphicsBeginImageContext(rect.size);
+    [img drawInRect:rect];
+    img = UIGraphicsGetImageFromCurrentImageContext();
+    
+    photoImageView.image = img;
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void) imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [_nickNameTextField resignFirstResponder];
+    return YES;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
--- Skea/PersonController/TestingViewController.m
+++ Skea/PersonController/TestingViewController.m
@@ -8,7 +8,7 @@
 
 #import "TestingViewController.h"
 
-@interface TestingViewController ()
+@interface TestingViewController ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate>
 
 @end
 
@@ -27,7 +27,139 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.navigationItem.titleView = [[Theam currentTheam] navigationTitleViewWithTitle:nil];
+    self.navigationItem.leftBarButtonItem = [[Theam currentTheam] navigationBarLeftButtonItemWithImage:IMG(@"back-cross.png") Title:nil Target:self Selector:@selector(btBack_DisModal:)];
+    
+    UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, self.view.frame.size.height) style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
 }
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row == 0)
+        return 60.f;
+    if(indexPath.row == 1)
+        return 60.f;
+    if(indexPath.row == 2)
+        return 40.f;
+    return 400.f;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString * cellIdentifier = [NSString stringWithFormat:@"cell_%ld",(long)indexPath.row];
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.backgroundColor = [UIColor whiteColor];
+    cell.contentView.backgroundColor = [UIColor whiteColor];
+    if(indexPath.row == 0)
+    {
+        UIView * lineView = [[UIView alloc] init];
+        lineView.frame = CGRectMake(0, 20, self.view.frame.size.width, 3);
+        lineView.backgroundColor = [UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1.f];
+        [cell.contentView addSubview:lineView];
+        
+        UILabel * roundLabel = [[UILabel alloc] init];
+        roundLabel.frame = CGRectMake(40, -7.5, 15, 15);
+        roundLabel.backgroundColor = [UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1.f];
+        roundLabel.text = @"1";
+        roundLabel.font = [UIFont systemFontOfSize:13];
+        roundLabel.layer.cornerRadius = 7.5;
+        roundLabel.layer.masksToBounds = YES;
+        roundLabel.textColor = [UIColor whiteColor];
+        roundLabel.textAlignment = NSTextAlignmentCenter;
+        [lineView addSubview:roundLabel];
+        
+        UILabel * roundLabel2 = [[UILabel alloc] init];
+        roundLabel2.frame = CGRectMake(self.view.frame.size.width - 43, -7.5, 15, 15);
+        roundLabel2.backgroundColor = [UIColor blueColor];
+        roundLabel2.text = @"2";
+        roundLabel2.font = [UIFont systemFontOfSize:13];
+        roundLabel2.layer.cornerRadius = 7.5;
+        roundLabel2.layer.masksToBounds = YES;
+        roundLabel2.textColor = [UIColor whiteColor];
+        roundLabel2.textAlignment = NSTextAlignmentCenter;
+        [lineView addSubview:roundLabel2];
+        
+        UILabel * label = [[UILabel alloc] init];
+        label.frame = CGRectMake(self.view.frame.size.width - 80, lineView.frame.origin.y + lineView.frame.size.height + 8, 70, 20);
+        label.backgroundColor = [UIColor clearColor];
+        label.textColor = [UIColor blueColor];
+        label.font = [UIFont systemFontOfSize:13];
+        label.text = NSLocalizedString(@"盆底肌测试", nil);
+        [cell.contentView addSubview:label];
+
+    }
+    else if (indexPath.row == 1)
+    {
+        UILabel * label = [[UILabel alloc] init];
+        label.frame = CGRectMake(20, 0, self.view.frame.size.width - 40, 60.f);
+        label.backgroundColor = [UIColor clearColor];
+        label.text = NSLocalizedString(@"请填写如下信息，有助于我们帮主您评估盆底肌健康状况", nil);
+        label.textColor = [UIColor blueColor];
+        label.numberOfLines = 2;
+        label.font = [UIFont systemFontOfSize:14];
+        [cell.contentView addSubview:label];
+    }
+    else if (indexPath.row == 2)
+    {
+        [cell.contentView addSubview:[self getTitleViewWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 40) title:@"基础数据"]];
+    }
+    else if (indexPath.row == 3)
+    {
+        
+    }
+    else
+    {
+        
+    }
+    return cell;
+}
+
+-(UIView *)getTitleViewWithFrame:(CGRect)frame title:(NSString *)title
+{
+    UIView * view = [[UIView alloc] init];
+    view.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+    view.backgroundColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1.f];
+    UIView * line = [[UIView alloc] init];
+    line.frame = CGRectMake(0, 0, 6, frame.size.height);
+    line.backgroundColor = [UIColor blueColor];
+    [view addSubview:line];
+    UILabel * label = [[UILabel alloc]init];
+    label.frame = CGRectMake(20, 0, view.frame.size.width - 20, frame.size.height);
+    label.backgroundColor = [UIColor clearColor];
+    label.font = [UIFont systemFontOfSize:15];
+    label.textColor = [UIColor blueColor];
+    label.text = title.length?title:@"";
+    [view addSubview:label];
+    return view;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    return YES;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
