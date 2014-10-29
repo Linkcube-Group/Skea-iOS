c69514ed26c1301fc22830b6d917ea6ae866cff8
yangmaosheng <yangmaosheng@techwolf.cn>
2014-10-29 15:42:47 +0800

    add chat

--- Skea/RecordController/RecordViewController.m
+++ Skea/RecordController/RecordViewController.m
@@ -8,10 +8,16 @@
 
 #import "RecordViewController.h"
 #import "CalendarView.h"
+#import "MPGraphView.h"
+#import "MPPlot.h"
+#import "MPBarsGraphView.h"
 
 @interface RecordViewController ()<CalendarDelegate>
 {
     CalendarView *_sampleView;
+    
+    MPGraphView *graph1;
+    MPBarsGraphView *graph2;
 }
 @end
 
@@ -41,7 +47,44 @@
     [self.view addSubview:_sampleView];
     _sampleView.hidden = YES;
     
+    ////////////////////////////////////////////////////////////////////
+    graph1=[[MPGraphView alloc] initWithFrame:CGRectMake(0, 300, 320, 150)];
+    graph1.waitToUpdate=YES;
+    
+    [graph1 setAlgorithm:^CGFloat(CGFloat x) {
+        return rand() % 100;
+    } numberOfPoints:20];
+    
+    graph1.curved=YES;
+    
+    graph1.graphColor=[UIColor colorWithRed:0.500 green:0.158 blue:1.000 alpha:1.000];
+    graph1.detailBackgroundColor=[UIColor colorWithRed:0.444 green:0.842 blue:1.000 alpha:1.000];
+    
+    graph2=[MPPlot plotWithType:MPPlotTypeBars frame:graph1.frame];
+    graph2.waitToUpdate=YES;
+    graph2.detailView=(UIView <MPDetailView> *)[self customDetailView];
+    [graph2 setAlgorithm:^CGFloat(CGFloat x) {
+        return rand() % 100;
+    } numberOfPoints:20];
+    graph2.graphColor=[UIColor blueColor];
+    [self.view addSubview:graph2];
+    
+    [self.view addSubview:graph1];
     // Do any additional setup after loading the view from its nib.
+}
+- (void)viewDidAppear:(BOOL)animated{
+    
+    [super viewDidAppear:animated];
+    
+    [self performSelector:@selector(animate) withObject:nil afterDelay:0.3];
+    
+}
+
+- (void)animate{
+    
+    [graph1 animate];
+    [graph2 animate];
+    
 }
 
 - (void)dateActoin
@@ -53,6 +96,23 @@
 {
     _sampleView.hidden = YES;
     NSLog(@"tappedOnDate %@(GMT)",selectedDate);
+}
+
+#pragma mark -
+#pragma mark Chat
+- (UIView *)customDetailView{
+    
+    UILabel* label=[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 35, 35)];
+    label.textAlignment=NSTextAlignmentCenter;
+    label.textColor=[UIColor blueColor];
+    label.backgroundColor=[UIColor whiteColor];
+    label.layer.borderColor=label.textColor.CGColor;
+    label.layer.borderWidth=.5;
+    label.layer.cornerRadius=label.width*.5;
+    label.adjustsFontSizeToFitWidth=YES;
+    label.clipsToBounds=YES;
+    
+    return label;
 }
 
 - (void)didReceiveMemoryWarning
