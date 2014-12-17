f201e205f4b8bad1107d1f271554d0bb5a33d362
zhengfuzhe2013 <yy_you_xiang@163.com>
2014-12-17 23:39:08 +0800

    bf

--- Skea/Models/SkeaUser.h
+++ Skea/Models/SkeaUser.h
@@ -9,8 +9,8 @@
 #import <Foundation/Foundation.h>
 
 typedef enum {
-    SpeedTypeChange = 0,      //变速
-    SpeedTypeConstant = 1     //恒速
+    SpeedTypeConstant = 0,     //恒速
+    SpeedTypeChange = 1        //变速
 }SpeedType;
 
 @interface SkeaUser : NSObject<NSCoding>
